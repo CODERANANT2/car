63c393594ae4d68fada38ff68a2dd3f8dd955b37
CODERANANT2 <72502773+CODERANANT2@users.noreply.github.com>
2021-03-23 13:49:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var gameState=0;
+var playerCount;
+var form,player,game;
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database()
+   
+}
+
+function draw(){
+    background("white");
+
+    drawSprites();
+}
+
